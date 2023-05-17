1990704de43dc770644273df6222bd9f78831702
Konstantin Lapshin <kostiantyn.lapshyn@dataart.com>
2023-05-17 17:57:28 +0300

    Added Employee class to extend UserIdentity. Added LeaveType and LeaveAllocation models with corresponding migration files.

---
+++ LeaveManagement.Web/Data/LeaveType.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    public abstract class LeaveType : BaseEntity
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+    }
+}
